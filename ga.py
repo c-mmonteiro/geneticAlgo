e7080eb2fd65c7f6995d30d5ee37ca8db9895d4e
Carlos Monteiro <carlos.monteiro@ifc.edu.br>
2025-03-14 03:45:05 -0300

    Rodando, mas com problema na seleção

--- ga.py
+++ ga.py
@@ -4,9 +4,10 @@ import struct
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 class AlgoritmoGenetico:
-    def __init__(self, tamanho_populacao, prob_mutacao, prob_cross, tipo_selecao, tolerancia_media_fitness):
+    def __init__(self, tamanho_populacao, prob_mutacao, prob_cross, tipo_selecao, tolerancia_media_fitness, min_geracao):
         #Tipo seleção: 'roleta', 'classificacao', 'torneio'
 
         self.prob_mutacao = prob_mutacao
@@ -18,7 +19,7 @@ class AlgoritmoGenetico:
 
         self.tamanho_cromossomo = 4 * 8 # size of chromossome in bits
 
-        self.populacao = pd.DataFrame(['Cromossomo', 'Fitness'])
+        self.populacao = pd.DataFrame(columns=['Cromossomo', 'Fitness'])
 
         media_fitness = []
         historico_populacao = []
@@ -29,9 +30,7 @@ class AlgoritmoGenetico:
             for i2 in range(self.tamanho_cromossomo):
                 bits += '1' if random.random() > 0.5 else '0'
             fitness = self.funcao_fitness(self.get_float(bits))
-            self.populacao = pd.concat([self.populacao, 
-                                        pd.DataFrame({'Cromossomo': bits,
-                                                      'Fitness': fitness})], ignore_index=True)
+            self.populacao.loc[len(self.populacao)] = [bits, fitness]
               
         historico_populacao.append(self.populacao)
         media_fitness.append(self.populacao['Fitness'].mean())
@@ -42,48 +41,82 @@ class AlgoritmoGenetico:
             lista_pais = self.selecao()
 
             #Nova era
-            self.populacao = pd.DataFrame(['Cromossomo', 'Fitness'])
+            self.populacao = pd.DataFrame(columns=['Cromossomo', 'Fitness'])
 
-            for ind in range(len(lista_pais)/2):
-                filhos = self.crossover([int(random.uniform(0,1)*self.tamanho_cromossomo)],
+            for ind in range(int(len(lista_pais)/2)):
+                filhos = self.crossover([int(random.uniform(0, self.tamanho_cromossomo))],
                             lista_pais[ind*2], lista_pais[ind*2+1])
-                
                 filho_1 = self.mutacao(filhos[0])
                 filho_2 = self.mutacao(filhos[1])
                 
-                self.populacao = pd.concat([self.populacao, 
-                                            pd.DataFrame({'Cromossomo': filho_1,
-                                                        'Fitness': self.funcao_fitness(self.get_float(filho_1))})], ignore_index=True)
-                self.populacao = pd.concat([self.populacao, 
-                                            pd.DataFrame({'Cromossomo': filho_2,
-                                                        'Fitness': self.funcao_fitness(self.get_float(filho_2))})], ignore_index=True)
+                self.populacao.loc[len(self.populacao)] = [filho_1, self.funcao_fitness(self.get_float(filho_1))]
+                self.populacao.loc[len(self.populacao)] = [filho_2, self.funcao_fitness(self.get_float(filho_2))]
             
             media_fitness_atual = self.populacao['Fitness'].mean()
             media_fitness.append(media_fitness_atual)
             historico_populacao.append(self.populacao)
-            if (media_fitness_atual - (sum(media_fitness)/len(media_fitness)) < tolerancia_media_fitness):
+            print(f'Geração {len(media_fitness)}')
+            #if ((media_fitness_atual - (sum(media_fitness[len(media_fitness)-10:])/10) < tolerancia_media_fitness) and
+            if(    len(media_fitness) >= min_geracao):
                 break
 
-        plt.plot(x, y)
+        print(self.populacao)
+
+        plt.subplot(2, 1, 1)
+        plt.plot(media_fitness)
         plt.title('Média do Fitness de cada população')
         plt.xlabel('População')
         plt.ylabel('Valor do Fitness')
+
+        pontos_fit = np.linspace(0, np.pi, num=1000)
+        funcao_fit = []
+        for ponto in pontos_fit:
+            funcao_fit.append(self.funcao_fitness(ponto))
+
+        cromossomo_float = []
+        for cromo in self.populacao['Cromossomo']:
+            cromossomo_float.append(self.get_float(cromo))
+
+        plt.subplot(2, 1, 2)
+        plt.plot(cromossomo_float, self.populacao['Fitness'], marker='o', linestyle='', color='b', markersize=5)
+        plt.plot(pontos_fit, funcao_fit, color='k')
+        plt.title('Última população')
+        plt.xlabel('Fitness')
+        plt.ylabel('Fit da População')
+        plt.xlim(0, 4)
+
+        
         plt.show()
 
             
 
-    def funcao_fitnees(self, y):
+    def funcao_fitness(self, y):
         if ((y < 0) or (y >= math.pi)):
             return 0
         else:
-            return y + abs(32*math.sin(y))
+            return y + abs(math.sin(32*y))
         
     def selecao(self):
         escolhidos_procriar = []
 
         if (self.tipo_selecao == 'roleta'):
             for i in range(self.quantidade_selecao):
-                escolhidos_procriar.append(self.populacao[(random.random()*self.tamanho_populacao)-1]['Cromossomo'])
+                escolhidos_procriar.append(self.populacao[int(random.uniform(0, self.tamanho_populacao-1))]['Cromossomo'])
+
+        elif (self.tipo_selecao == 'roleta_prob'):
+            soma = self.populacao['Fitness'].sum()
+            for i in range(self.quantidade_selecao):
+                prob = random.uniform(0, 1)
+                contador = 0
+                for ind, fit in enumerate(self.populacao['Fitness']):
+                    contador += fit/soma
+                    #print(f'Contador {contador} soma {soma}')
+                    if contador >= prob:
+                        print(i)
+                        escolhidos_procriar.append(self.populacao.iloc[ind]['Cromossomo'])
+                        break
+                    #TODO: Problemas aqui!                
+
 
         elif (self.tipo_selecao == 'classificacao'):
             populacao_classificada = self.populacao.sort_values(by='Fitness', ascending=False).head(self.quantidade_selecao)
@@ -91,8 +124,8 @@ class AlgoritmoGenetico:
 
         elif (self.tipo_selecao == 'torneio'):
             for i in range(self.quantidade_selecao):
-                pessoa_1 = self.populacao[(random.random()*self.tamanho_populacao)-1]
-                pessoa_2 = self.populacao[(random.random()*self.tamanho_populacao)-1]
+                pessoa_1 = self.populacao.iloc[int(random.uniform(0, self.tamanho_populacao-1))]
+                pessoa_2 = self.populacao.iloc[int(random.uniform(0, self.tamanho_populacao-1))]
 
                 p = pessoa_1['Fitness'] / (pessoa_1['Fitness'] + pessoa_2['Fitness'])
 
@@ -122,6 +155,7 @@ class AlgoritmoGenetico:
                         else:
                             filho_1 += cromossomo_pai[ultimo_corte:corte]
                             filho_2 += cromossomo_mae[ultimo_corte:corte]
+                        
                     
                     if(i == len(pontos_corte)-1):
                         if (i % 2 != 0):
@@ -135,9 +169,9 @@ class AlgoritmoGenetico:
                 else:
                     print(f'ERRO: Valor da posição de corte no Crossover é maior que o cromossomo -> Pontos de corte: {pontos_corte}')
         
-            else:
-                filho_1 = cromossomo_mae
-                filho_2 = cromossomo_pai
+        else:
+            filho_1 = cromossomo_mae
+            filho_2 = cromossomo_pai
         return [filho_1, filho_2]   
             
 
@@ -146,14 +180,15 @@ class AlgoritmoGenetico:
         novo_cromossomo = ''
         numero_aleatorio = random.random()
         if (numero_aleatorio > self.prob_mutacao):
-            for i, bit in enumerate(cromossomo):
-                novo_cromossomo += '1' if bit == '0' else '0'
+            posicao = int(random.uniform(0, self.tamanho_cromossomo-1))
+            novo_cromossomo = cromossomo[0:posicao] + ('1' if cromossomo[posicao] == '0' else '0') + cromossomo[posicao+1:]
+
+            #Inverte todos os bits
+            #for i, bit in enumerate(cromossomo):
+            #    novo_cromossomo += '1' if bit == '0' else '0'
+        else:
+            novo_cromossomo = cromossomo
         return novo_cromossomo
-
-
-    def gerarPopulacao1(self, anos):
-        self.idade += anos
-        return f"{self.nome} agora tem {self.idade} anos."
     
 ###########################################################
 ######      Conversões numericas (Prof. Eric)
@@ -186,5 +221,4 @@ class AlgoritmoGenetico:
         return self.bitsToFloat(x)
 ######################################################
 
-AlgoritmoGenetico(100, 0.01, 0.7, 'torneio', 0.05)
-
+AlgoritmoGenetico(500, 0.01, 0.7, 'roleta_prob', 0.001, 500)
